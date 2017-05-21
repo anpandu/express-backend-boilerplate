3a53b328f3f794590b055dc331bd259368b317a8
ananta pandu <anpandumail@gmail.com>
2017-05-21 16:05:21 +0700

    example repository

--- modules/config.js
+++ modules/config.js
@@ -2,9 +2,16 @@ require('dotenv').config()
 
 const config = {
   server: {},
+  db: {
+    host: process.env.DB_HOST || '',
+    port: process.env.DB_PORT || '',
+    db: process.env.DB_NAME || '',
+    user: process.env.DB_USER || '',
+    pass: process.env.DB_PASS || '',
+  },
   logger: {
-    isFile: process.env.PAPERTRAIL_IS_FILE || 'false',
-    isPapertrail: process.env.PAPERTRAIL_IS_FILE || 'false',
+    isFile: process.env.IS_FILE || 'false',
+    isPapertrail: process.env.IS_PAPERTRAIL || 'false',
     papertrail: {
       host: process.env.PAPERTRAIL_HOST || 'host',
       port: process.env.PAPERTRAIL_PORT || 'port',
