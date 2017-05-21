7da001b661eaffa75d4b712d2f8a395ad45885ed
ananta pandu <anpandumail@gmail.com>
2017-05-21 14:45:05 +0700

    init

---
+++ modules/app.js
@@ -0,0 +1,32 @@
+
+
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+
+var routes = require('./handlers/index')
+
+var app = express()
+
+var env = process.env.NODE_ENV || 'development'
+app.locals.ENV = env
+app.locals.ENV_DEVELOPMENT = env == 'development'
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+
+app.use('/', routes)
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+module.exports = app
